13807d743e38bd8fa2bed1c33f69e1d313163794
Robert Gumeny <robert.gumeny528@gmail.com>
2020-07-16 01:34:32 -0600

    tutorial complete

--- pokemon-graphql/src/containers/PokemonContainer.js
+++ pokemon-graphql/src/containers/PokemonContainer.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import { Pokemon } from '../components/Pokemon';
+import { GET_POKEMONS } from '../graphql/get-pokemons';
 
 export function PokemonContainer() {
+  // NOTE Destructuring, look into this more!
+  const { data: { pokemons = [] } = {} } = useQuery(GET_POKEMONS, {
+    variables: { first: 9 },
+  });
+
   return (
-    <p>I am a pokemon container</p>
+    <div className="container">
+      {pokemons && pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)}
+    </div>
   )
 }
